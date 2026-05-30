7d7951a7aa7cffee5041ac803649bfc505804601
nanka1570 <nanka1570@gmail.com>
2026-05-30 23:05:39 +0900

    refactor: selection.tsの旧コードコメントを意図説明に置き換え

--- React/Projects/src/lib/selection.ts
+++ React/Projects/src/lib/selection.ts
@@ -1,12 +1,4 @@
 // チェックボックス
-// if (selectedCheckboxCategoryId.includes(category.id)) {
-//     setSelectedCheckboxCategoryId(
-//         selectedCheckboxCategoryId.filter((id) => id !== category.id)
-//     )
-// } else {
-//     setSelectedCheckboxCategoryId(
-//         [...selectedCheckboxCategoryId, category.id]
-//     )
-// }
+// idが配列にあれば外す、なければ足す（チェックボックスのON/OFF切替）
 export const toggleId = (ids: number[], id: number): number[] => 
     ids.includes(id) ? ids.filter((x) => x !== id) : [...ids, id]
